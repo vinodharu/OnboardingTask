cd31d8b26eedc6437d6f67420b62fa54431509cb
Bhavatharani Govind <bhavatharani_govind@epam.com>
2025-02-25 17:50:16 +0530

    Add project files.

---
+++ OnboardingTask/Logger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using log4net;
+using log4net.Config;
+
+namespace SaucedemoAutomation
+{
+    [TestFixture]
+    public class AddToCartTest
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(AddToCartTest));
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            XmlConfigurator.Configure();
+            log.Info("Setting up the WebDriver and navigating to the site.");
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+        }
+
+        [Test]
+        public void AddItemToCartTest()
+        {
+            log.Info("Attempting login.");
+
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+
+            Assert.That(driver.Url, Does.Contain("inventory.html"), "Login failed.");
+            log.Info("Login successful.");
+
+            log.Info("Adding an item to the cart.");
+            IWebElement addToCartButton = driver.FindElement(By.CssSelector("button[name='add-to-cart-sauce-labs-backpack']"));
+            addToCartButton.Click();
+
+            IWebElement cartBadge = driver.FindElement(By.ClassName("shopping_cart_badge"));
+            Assert.That(cartBadge.Text, Is.EqualTo("1"), "Item was not added to the cart.");
+            log.Info("Item successfully added to the cart.");
+
+            log.Info("Navigating to the cart.");
+            driver.FindElement(By.ClassName("shopping_cart_link")).Click();
+
+            Assert.That(driver.Url, Does.Contain("cart.html"), "Failed to navigate to cart page.");
+            log.Info("Cart page loaded successfully.");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            log.Info("Closing the WebDriver.");
+            driver.Quit();
+        }
+    }
+}
---
+++ OnboardingTask/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SaucedemoAutomation
+{
+    [TestFixture]
+    public class LoginTest
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+        }
+
+        [Test]
+        public void ValidLoginTest()
+        {
+            string[] validUsers = { "standard_user", "locked_out_user", "problem_user", "performance_glitch_user" };
+
+            Array.Sort(validUsers);
+
+            Console.WriteLine("Sorted Usernames:");
+            foreach (var user in validUsers)
+            {
+                Console.WriteLine(user);
+            }
+
+            string selectedUser = "standard_user";
+            int index = Array.IndexOf(validUsers, selectedUser);
+
+            Assert.That(index, Is.GreaterThanOrEqualTo(0), $"User {selectedUser} not found in the list.");
+
+            IWebElement usernameField = driver.FindElement(By.Id("user-name"));
+            IWebElement passwordField = driver.FindElement(By.Id("password"));
+            IWebElement loginButton = driver.FindElement(By.Id("login-button"));
+
+            usernameField.SendKeys(selectedUser);
+            passwordField.SendKeys("secret_sauce");
+            loginButton.Click();
+
+            Assert.That(driver.Url, Does.Contain("inventory.html"), "Login failed.");
+
+            Console.WriteLine($"User {selectedUser} logged in successfully.");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
